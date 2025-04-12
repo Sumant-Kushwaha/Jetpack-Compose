f2e61600eb4be31aea12809867fe510ac214fcc1
Sumant <SumantKushwaha.dev@gmail.com>
2025-04-12 20:17:59 +0530

    Text Composable Update

--- ComposeComponent/app/src/main/java/com/amigo/composecomponent/Text.kt
+++ ComposeComponent/app/src/main/java/com/amigo/composecomponent/Text.kt
@@ -12,47 +12,49 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 
-var fontFamily= FontFamily(
-    Font(R.font.lexend_light, FontWeight.Light),
+val lexendFont = FontFamily(
     Font(R.font.lexend_bold, FontWeight.Bold),
-    Font(R.font.lexend_black, FontWeight.Black),
-    Font(R.font.lexend_regular, FontWeight.Normal),
-    Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
     Font(R.font.lexend_thin, FontWeight.Thin),
-    Font(R.font.lexend_semibold, FontWeight.SemiBold),
+    Font(R.font.lexend_light, FontWeight.Light),
+    Font(R.font.lexend_black, FontWeight.Black),
     Font(R.font.lexend_medium, FontWeight.Medium),
+    Font(R.font.lexend_regular, FontWeight.Normal),
+    Font(R.font.lexend_semibold, FontWeight.SemiBold),
+    Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
     Font(R.font.lexend_extralight, FontWeight.ExtraLight),
 )
 
-
 @Composable
-fun TextStyling(){
+fun TextStyling() {
     Text(
-        text = buildAnnotatedString {
+        buildAnnotatedString {
             withStyle(
                 style = SpanStyle(
-                    Color.Red,
-                    fontSize = 30.sp,
                     fontStyle = FontStyle.Italic,
-                    fontWeight = FontWeight.Bold
+                    fontFamily = lexendFont,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.Red,
+                    fontSize = 50.sp
                 )
-            ){
-                append("O")
+            ) {
+                append("S")
             }
-             append("mshree")
+            append("umant")
             withStyle(
                 style = SpanStyle(
-                    Color.Red,
-                    fontSize = 30.sp,
                     fontStyle = FontStyle.Italic,
-                    fontWeight = FontWeight.Bold
+                    fontFamily = lexendFont,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.Red,
+                    fontSize = 50.sp
                 )
-            ){
-                append(" K")
+            )
+            {
+                append("K")
             }
-             append("ushwaha")
+            append("ushwaha")
         },
-        fontSize = 20.sp,
-        color=Color.Green
+        color = Color.Green,
+        fontSize = 30.sp
     )
 }
