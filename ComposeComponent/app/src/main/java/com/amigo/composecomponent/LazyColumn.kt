10e14612f9d739873afaeb798f2cd9888045b86b
Sumant <SumantKushwaha.dev@gmail.com>
2025-04-18 08:44:53 +0530

    Lazy Column

--- ComposeComponent/app/src/main/java/com/amigo/composecomponent/LazyColumn.kt
+++ ComposeComponent/app/src/main/java/com/amigo/composecomponent/LazyColumn.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -37,12 +39,20 @@ fun PreviewFunction() {
 //        Box(Modifier.padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()))
 //        BlogCategory(R.drawable.ic_profile, "Programmer", "Learning Different Languages")
 //    }
-    Column (
-        Modifier.verticalScroll(rememberScrollState())
-    ) {
+//    Column (
+//        Modifier.verticalScroll(rememberScrollState())
+//    ) {
+//        Box(Modifier.padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()))
+//        getCategoryList().map { item->
+//            BlogCategory(item.img,item.title,item.subTitle)
+//        }
+//    }
+    Column {
         Box(Modifier.padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()))
-        getCategoryList().map { item->
-            BlogCategory(item.img,item.title,item.subTitle)
+        LazyColumn {
+            items(getCategoryList()){item->
+                BlogCategory(item.img,item.title,item.subTitle)
+            }
         }
     }
 }
